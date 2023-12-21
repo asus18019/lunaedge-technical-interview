540f99b6471a6dfc3dc357b47c9ec2eb92c7d4db
Viktor Volokhan <voloxanvitya@gmail.com>
2023-12-21 10:23:15 +0200

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-
 function App() {
-  return (
-    <div>
-      <p>Welcome to Luna Edge technical interview</p>
-    </div>
-  )
+	return (
+		<div>
+			<h1 className="underline">Welcome to Luna Edge technical interview</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
